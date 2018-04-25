57ddfd4816bbd4dfb0519b6fd71afda9fdcb53be
commentatorboy <commentatorboy@gmail.com>
2018-04-25 17:01:39 +0200

    added nuget packets and database connection string

--- Server/M_Core/Data/DataContext.cs
+++ Server/M_Core/Data/DataContext.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using M_Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace M_Core.NewFolder
+namespace M_Core.Data
 {
-    public class DataContext
+   
+    public class DataContext : IdentityDbContext<ApplicationUser>
     {
+
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+            //     builder.Entity<ApplicationUser> ()
+            //    .HasMany (s => s.Groups);
+        }
     }
 }
