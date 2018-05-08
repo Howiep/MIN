c8f5e9e3168a8401f1bb3c01c71622849a5d086b
Howie <hawkarp@gmail.com>
2018-05-08 13:47:38 +0200

    choose date refactoring

--- Client/src/components/experience/steps/CreateStepper.vue
+++ Client/src/components/experience/steps/CreateStepper.vue
@@ -9,28 +9,8 @@
     </v-stepper-header>
     <v-stepper-items class="stepperCard">
       <v-stepper-content step="1">
-        <div>
-            <v-dialog ref="dateDialog" v-model="modal" :return-value.sync="date" persistent lazy full-width width="290px">
-              <v-text-field slot="activator" v-model="date" label="Vælg Dato" prepend-icon="event" readonly></v-text-field>
-              <v-date-picker v-model="date" scrollable>
-                <v-spacer></v-spacer>
-                <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
-                <v-btn flat color="primary" @click="$refs.dateDialog.save(date)">OK</v-btn>
-              </v-date-picker>
-            </v-dialog>
-        </div>
-        <div class="mb-5">
-           <v-dialog ref="timeDialog" persistent v-model="modal2" lazy full-width width="290px" :return-value.sync="time">
-            <v-text-field slot="activator" label="Vælg Tid" v-model="time" prepend-icon="access_time" readonly
-            ></v-text-field>
-            <v-time-picker v-model="time" actions>
-              <v-spacer></v-spacer>
-              <v-btn flat color="primary" @click="modal2 = false">Cancel</v-btn>
-              <v-btn flat color="primary" @click="$refs.timeDialog.save(time)">OK</v-btn>
-            </v-time-picker>
-          </v-dialog>
-        </div>
-        <p v-if="dateFormatted">Du har valgt: {{ dateFormatted }} - {{ time }}</p>
+        <choose-date v-on:setDate="setDate"></choose-date>
+        <p v-if="dateFormatted">Du har valgt: {{ dateFormatted }}</p>
         <v-btn color="primary" @click.native="e1 = 2">Fortsæt</v-btn>
         <v-btn flat @click="closeMenu()">Annuller</v-btn>
       </v-stepper-content>
@@ -58,50 +38,32 @@
 
 <script>
 import ChooseExperience from '@/components/experience/steps/ChooseExperience'
+import ChooseDate from '@/components/experience/steps/ChooseDate'
 
 export default {
   name: 'createStepper',
   data () {
     return {
       e1: 1,
-      date: null,
-      time: null,
       createNote: '',
       selectedExperiences: [],
-      dateFormatted: null,
-      modal: false,
-      modal2: false
-    }
-  },
-  computed: {
-    computedDateFormatted () {
-      return this.formatDate(this.date)
-    }
-  },
-  watch: {
-    time () {
-      this.dateFormatted = this.formatDate(this.date)
+      dateFormatted: null
     }
   },
   methods: {
     closeMenu () {
       this.$emit('closeMenu', this.dialog)
     },
-    chooseTime () {
-      this.dateFormatted = this.date + ' ' + this.time
-    },
-    formatDate (date) {
-      if (!date) return null
-
-      const [year, month, day] = date.split('-')
-      return `${day}/${month}/${year}`
-    },
     setExperiences (selected) {
       this.selectedExperiences = selected
+    },
+    setDate (date) {
+      this.dateFormatted = date
     }
   },
   components: {
-    ChooseExperience
+    ChooseExperience,
+    ChooseDate
   }
 }
 </script>
