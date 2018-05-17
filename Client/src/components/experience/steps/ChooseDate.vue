71fa97ebb2db0ed1dd8efa1a7e8c28e5bba65f1e
Howie <hawkarp@gmail.com>
2018-05-17 15:50:05 +0200

    choosedate

--- Client/src/components/experience/steps/ChooseDate.vue
+++ Client/src/components/experience/steps/ChooseDate.vue
@@ -3,7 +3,7 @@
    <div>
         <v-dialog ref="dateDialog" v-model="modal" :return-value.sync="date" persistent lazy full-width width="290px">
           <v-text-field slot="activator" v-model="date" label="Vælg Dato" prepend-icon="event" readonly></v-text-field>
-          <v-date-picker v-model="date" scrollable>
+          <v-date-picker v-model="date" no-title scrollable>
             <v-spacer></v-spacer>
             <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
             <v-btn flat color="primary" @click="$refs.dateDialog.save(date)">OK</v-btn>
