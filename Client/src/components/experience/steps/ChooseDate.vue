c21062b60bb12ac63537d4f07b56a28fb6e890e5
Howie <hawkarp@gmail.com>
2018-05-17 17:57:10 +0200

    date set to today

--- Client/src/components/experience/steps/ChooseDate.vue
+++ Client/src/components/experience/steps/ChooseDate.vue
@@ -24,6 +24,25 @@ export default {
       modal: false
     }
   },
+  async mounted () {
+    try {
+      let today = new Date()
+      let dd = today.getDate()
+      let mm = today.getMonth()+1 //January is 0!
+      let yyyy = today.getFullYear()
+      if(dd<10) {
+          dd = '0'+dd
+      }
+
+      if(mm<10) {
+          mm = '0'+mm
+      }
+      today = yyyy + '-' + mm + '-' + dd
+      this.date = today
+    } catch (error) {
+      console.log(error)
+    }
+  },
   watch: {
     date () {
       this.dateFormatted = this.formatDate(this.date)
